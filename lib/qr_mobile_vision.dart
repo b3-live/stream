70bd253437ab7e8820a14a77f10c75b12aebdc96
SomeBody <somebody@someplace.com>
2022-09-12 23:30:03 -0400

    Squashed 'vendor/github.com/rmtmckenzie/flutter_qr_mobile_vision/' content from commit 8074fb2
    git-subtree-dir: vendor/github.com/rmtmckenzie/flutter_qr_mobile_vision
    git-subtree-split: 8074fb2f5395dd4eefa467066b24d9ea414f03e4

---
+++ lib/qr_mobile_vision.dart
@@ -0,0 +1,3 @@
+export 'package:qr_mobile_vision/src/barcode_formats.dart';
+export 'package:qr_mobile_vision/src/camera_direction.dart';
+export 'package:qr_mobile_vision/src/qr_mobile_vision.dart';
